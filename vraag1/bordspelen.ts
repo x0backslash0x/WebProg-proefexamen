35826e8b587ee7693303d18f639159ccb6741826
x0backslash0x <paddy0524@gmail.com>
2025-01-06 10:02:13 +0100

    project starten

---
+++ vraag1/bordspelen.ts
@@ -0,0 +1,60 @@
+/* bordspelen.ts
+ *
+ * 2024-2025 Patrick Lanove
+ * BA Electronica-ICT/Cybersecurity
+ * OLOD Web programming
+ *
+ * oefenexamen
+**/
+
+export let bordspelen = [
+  {
+    title: "Catan",
+    year: 1995,
+    expansions: ["Seafarers", "Cities & Knights"],
+    awards: ["Spiel des Jahres"],
+    adaptations: { type: "video game", year: 2005 }
+  },
+  {
+    title: "Pandemic",
+    year: 2008,
+    expansions: ["On the Brink", "In the Lab"],
+    awards: ["Golden Geek", "Spiel des Jahres"],
+    adaptations: { type: "video game", year: 2013 }
+  },
+  {
+    title: "Gloomhaven",
+    year: 2017,
+    expansions: ["Forgotten Circles"],
+    awards: ["Golden Geek"],
+    adaptations: {}
+  },
+  {
+    title: "Ticket to Ride - Europe",
+    year: 2005,
+    expansions: [],
+    awards: ["Spiel des Jahres"],
+    adaptations: { type: "video game", year: 2006 }
+  },
+  {
+    title: "Twilight Imperium - Fourth Edition",
+    year: 2017,
+    expansions: ["Prophecy of Kings"],
+    awards: ["Golden Geek"],
+    adaptations: {}
+  },
+  {
+    title: "Carcassonne",
+    year: 2000,
+    expansions: ["Inns & Cathedrals", "Traders & Builders"],
+    awards: ["Spiel des Jahres"],
+    adaptations: { type: "video game", year: 2012 }
+  },
+  {
+    title: "Azul",
+    year: 2017,
+    expansions: [],
+    awards: ["Spiel des Jahres", "Golden Geek"],
+    adaptations: {}
+  }
+]
